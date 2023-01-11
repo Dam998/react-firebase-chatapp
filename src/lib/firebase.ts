246286cfec4b0c4429fcea94350a06758c541996
Damiano <dpapi68@gmail.com>
2023-01-11 01:58:03 +0100

    Add env api key

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,6 +4,12 @@ import { getAuth, GoogleAuthProvider, signInWithRedirect, signOut as firebaseSig
 
 const firebaseConfig = {
     // Firebase configurations
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: 'react-chatapp-eca7f.firebaseapp.com',
+    projectId: 'react-chatapp-eca7f',
+    storageBucket: 'react-chatapp-eca7f.appspot.com',
+    messagingSenderId: '719330437939',
+    appId: '1:719330437939:web:fa93289b1f3eaad0a5ec46'
 };
 
 // Initialize Firebase
